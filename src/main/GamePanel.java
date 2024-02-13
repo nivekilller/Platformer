8d612cab86fab186f293c2a2931439ce470d1c1a
nivekilller <kevin@mysteiners.com>
2024-02-13 00:58:31 -0500

    Created the game loop that has a flat 120fps and moves the rectangle around the window.

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -2,13 +2,19 @@ package main;
 import inputs.KeyboardInputs;
 import inputs.MouseInputs;
 
-import java.awt.Graphics;
+import java.awt.*;
+import java.util.Random;
 import javax.swing.*;
 
 public class GamePanel extends JPanel {
     private MouseInputs mouseInputs;
-    private int xDelta = 100, yDelta = 100;
-    public GamePanel(){
+    private float xDelta = 100, yDelta = 100;
+    private float xDir = 0.1f, yDir = 0.1f;
+    private Color color = new Color(255, 130, 12);
+    private Random random;
+
+    public GamePanel() {
+        random = new Random();
         mouseInputs = new MouseInputs(this);
         addKeyListener(new KeyboardInputs(this));
         addMouseListener(mouseInputs);
@@ -16,25 +22,48 @@ public class GamePanel extends JPanel {
 
     }
 
-    public void changeXDelta(int value){
+    public void changeXDelta(int value) {
         this.xDelta += value;
         repaint();
     }
 
-    public void changeYDelta(int value){
+    public void changeYDelta(int value) {
         this.yDelta += value;
         repaint();
     }
 
-    public void setRectPosition(int x, int y){
+    public void setRectPosition(int x, int y) {
         this.xDelta = x;
         this.yDelta = y;
         repaint();
     }
 
-    public void paintComponent(Graphics g){
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.fillRect(xDelta,yDelta,200,50);
+        updateRectangle();
+        g.setColor(color);
+        g.fillRect((int) xDelta, (int) yDelta, 200, 50);
+        repaint();
     }
 
+    private void updateRectangle() {
+        xDelta += xDir;
+        if (xDelta > 400 || xDelta < 0) {
+            xDir *= -1;
+            color = getRandColor(); }
+
+        yDelta += yDir;
+        if (yDelta > 400 || yDelta < 0) {
+            yDir *= -1;
+            color = getRandColor();
+        }
+    }
+
+    private Color getRandColor(){
+        int r = random.nextInt(0, 255);
+        int g = random.nextInt(0, 255);
+        int b = 0; random.nextInt(0, 255);
+
+        return new Color(r, g, b);
+    }
 }
